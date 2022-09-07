94ce09cf7aaf1ca7c5748efbd00e9ac4d6de4853
AlekseyChirkin <ach1010110@yandex.ru>
2022-09-07 11:23:27 +0300

    refactor task 5

--- Tasks/task5.py
+++ Tasks/task5.py
@@ -2,41 +2,29 @@
 # Заменить все цифры соответствующими словами.
 
 fileName = 'txt/textForTask_5.txt'
-capitalizedEveryWordText = ''
-allNumsAsWordsText = ''
+numbersAsWords = {
+    '0': 'Ноль',
+    '1': 'Один',
+    '2': 'Два',
+    '3': 'Три',
+    '4': 'Четыре',
+    '5': 'Пять',
+    '6': 'Шесть',
+    '7': 'Семь',
+    '8': 'Восемь',
+    '9': 'Девять'
+}
+contentResult = ''
 
 with open(fileName, encoding='utf8') as file:
     lines = file.readlines()
 
-lines = str(lines).split(' ')
-for word in lines:
-    capitalizedEveryWordText += word.capitalize() + ' '
+lines = str(lines).title()
 
-for char in capitalizedEveryWordText:
-    if char == '0':
-        char = ' Ноль'
-    if char == '1':
-        char = 'Один '
-    if char == '2':
-        char = ' Два'
-    if char == '3':
-        char = ' Три'
-    if char == '4':
-        char = ' Четыре'
-    if char == '5':
-        char = ' Пять'
-    if char == '6':
-        char = ' Шесть'
-    if char == '7':
-        char = ' Семь'
-    if char == '8':
-        char = ' Восемь'
-    if char == '9':
-        char = ' Девять'
+for char in lines:
+    if char.isdigit():
+        char = ' ' + numbersAsWords[char]
+    contentResult += char
 
-    allNumsAsWordsText += char
-
-for i in range(0, 3):
-    allNumsAsWordsText.replace('  ', ' ')
-
-print(allNumsAsWordsText)
+contentResult = contentResult.strip().replace('  ', ' ').replace('  ', ' ')
+print(contentResult)
