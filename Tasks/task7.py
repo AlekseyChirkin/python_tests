68a52f4c8392b8a6cb25ca838a80de241c9b75c9
AlekseyChirkin <ach1010110@yandex.ru>
2022-09-07 12:50:47 +0300

    solving task 7

--- Tasks/task7.py
+++ Tasks/task7.py
@@ -3,3 +3,42 @@
 # алфавитном порядке. Найдите средний балл каждой группы по каждому предмету. Определите самого старшего студента и
 # самого младшего студента. Для каждой группы найдите лучшего с точки зрения успеваемости студента.
 
+class Student:
+
+    def __init__(self, name: str, year_of_birth: int, year: int, group: int, grade1: int, grade2: int, grade3: int,
+                 grade4: int, grade5: int):
+        self.name = name
+        self.year_of_birth = year_of_birth
+        self.year = year
+        self.group = group
+        self.grade1 = grade1
+        self.grade2 = grade2
+        self.grade3 = grade3
+        self.grade4 = grade4
+        self.grade5 = grade5
+
+    pass
+
+
+file_name = 'txt/students.txt'
+students_list = []
+
+with open(file_name, encoding='utf8') as file:
+    lines = file.readlines()
+
+for line in lines:
+    data_of_student = line.split()
+    student = Student(
+        data_of_student[0] + ' ' + data_of_student[1] + ' ' + data_of_student[2],
+        int(data_of_student[3]),
+        int(data_of_student[4]),
+        int(data_of_student[5]),
+        int(data_of_student[6]),
+        int(data_of_student[7]),
+        int(data_of_student[8]),
+        int(data_of_student[9]),
+        int(data_of_student[10]))
+    students_list.append(student)
+
+
+
--- Tasks/task7_file_generator.py
+++ Tasks/task7_file_generator.py
@@ -1,16 +1,15 @@
 # Генерация файла с данными для задачи №7
 import random
 
-fileInputName = "txt/names.txt"
-fileOutputName = "txt/students.txt"
-content = ""
+fileInputName = 'txt/names.txt'
+fileOutputName = 'txt/students.txt'
+content = ''
 
-with open(fileInputName, encoding="utf8") as file:
+with open(fileInputName, encoding='utf8') as file:
     names = file.readlines()
 
 for name in names:
-    name = name.removesuffix("\n")
-    content += name + " "\
+    content += name.strip() + " "\
         + str(1990 + round(random.random() * 20)) + " "\
         + str(round(random.random() * 4) + 1) + " "\
         + str(round(random.random() * 9) + 1) + " "\
@@ -19,8 +18,8 @@ for name in names:
         + str(round(random.random() * 3) + 2) + " "\
         + str(round(random.random() * 3) + 2) + " "\
         + str(round(random.random() * 3) + 2) + "\n"
-    content.removesuffix("\n")
+content = content.strip()
 
-with open(fileOutputName, "w", encoding="utf8") as file:
+with open(fileOutputName, 'w', encoding='utf8') as file:
     file.write(content)
-    print(f"The file \"{file.name}\" successfully created!")
+    print(f'The file "{file.name}" successfully created!')
