aab2320574e298808f7a2f2f2ea59b76ca93e997
root <ach1010110@yandex.ru>
2022-09-07 21:43:55 +0300

    solve task 7

--- Tasks/task7.py
+++ Tasks/task7.py
@@ -6,40 +6,10 @@
 import sys
 import Student
 
+content = ''
 
-def sort_students(students_list_for_sort):
-    # сортируем студентов по курсу и внутри курса по алфавиту
-    sorted_students = []
-    for i in range(1, 6):
-
-        students_this_year = []
-        for st in students_list_for_sort:
-            if st.year == i:
-                students_this_year.append(st)
-
-        students_this_year = sorted(students_this_year, key=lambda stu: stu.name)
-        for j in students_this_year:
-            sorted_students.append(j)
-    return sorted_students
-
-
-def print_average_grade_by_group(students):
-    for ye in range(1, 6):
-        for gr in range(1, 11):
-            count_of_students_in_group = 0
-            average_grade_in_group = 0
-            for s in students:
-                if s.year == ye and s.group == gr:
-                    average_grade_in_group += (s.grade1 + s.grade2 + s.grade3 + s.grade4 + s.grade5) / 5
-                    count_of_students_in_group += 1
-            if count_of_students_in_group != 0:
-                average_grade_in_group = average_grade_in_group / count_of_students_in_group
-                print(f'Средняя оценка в группе номер {gr}, {ye} курса, составила {average_grade_in_group} баллов')
-
-    pass
-
-
-file_name = 'txt/students20.txt'
+file_name = 'txt/students.txt'
+output_file_name = 'txt/task7_report.txt'
 students_list = []
 
 # читаем из файла
@@ -65,13 +35,106 @@ for line in lines:
         int(data_of_student[10]))
     students_list.append(student)
 
+
+def sort_students(students_list_for_sort):
+    # сортируем студентов по курсу и внутри курса по алфавиту
+    sorted_students = []
+    for i in range(1, 6):
+        students_this_year = []
+        for stu in students_list_for_sort:
+            if stu.year == i:
+                students_this_year.append(stu)
+
+        students_this_year = sorted(students_this_year, key=lambda stud: stud.name)
+        for j in students_this_year:
+            sorted_students.append(j)
+    return sorted_students
+
+
+def print_average_grade_by_group(students):
+    cont = ''
+    for ye in range(1, 6):
+        for gr in range(1, 11):
+            count_of_students_in_group = 0
+            average_grade_in_group = 0
+            for s in students:
+                if s.year == ye and s.group == gr:
+                    average_grade_in_group += (s.grade1 + s.grade2 + s.grade3 + s.grade4 + s.grade5) / 5
+                    count_of_students_in_group += 1
+            if count_of_students_in_group != 0:
+                average_grade_in_group = average_grade_in_group / count_of_students_in_group
+                print(f'Средняя оценка в группе номер {gr}, {ye} курса, составила {average_grade_in_group} баллов')
+                cont += f'Средняя оценка в группе номер {gr}, {ye} курса, составила {average_grade_in_group} баллов\n'
+    return cont
+
+
+def print_oldest_students(data_students):
+    cont = ''
+
+    oldest = data_students[0].year_of_birth
+    for stu in data_students:
+        if oldest > stu.year_of_birth:
+            oldest = stu.year_of_birth
+    for stu in data_students:
+        if oldest == stu.year_of_birth:
+            print(f'Старший студент среди всех: {stu.name}, {stu.year_of_birth} года рождения')
+            cont += f'Старший студент среди всех: {stu.name}, {stu.year_of_birth} года рождения\n'
+    return cont
+
+
+def print_young_students(data_students):
+    cont = ''
+
+    youngest = data_students[0].year_of_birth
+    for stu in data_students:
+        if youngest < stu.year_of_birth:
+            youngest = stu.year_of_birth
+    for stu in data_students:
+        if youngest == stu.year_of_birth:
+            print(f'Самый младший студент среди всех: {stu.name}, {stu.year_of_birth} года рождения')
+            cont += f'Самый младший студент среди всех: {stu.name}, {stu.year_of_birth} года рождения\n'
+    return cont
+
+
+def print_best_student_in_group(data_students):
+    cont = ''
+
+    for ye in range(1, 6):
+        for gr in range(1, 11):
+            best_average_grade_in_group = 0
+            for s in data_students:
+                if s.year == ye and s.group == gr:
+                    average_grade = (s.grade1 + s.grade2 + s.grade3 + s.grade4 + s.grade5) / 5
+                    if average_grade > best_average_grade_in_group:
+                        best_average_grade_in_group = average_grade
+            for s in data_students:
+                if s.year == ye and s.group == gr:
+                    average_grade = (s.grade1 + s.grade2 + s.grade3 + s.grade4 + s.grade5) / 5
+                    if average_grade == best_average_grade_in_group:
+                        print(f'Лидер по успеваемости в {s.group} группе,'
+                              f'{s.year} курса: {s.name}, средний балл {best_average_grade_in_group}')
+                        cont += (f'Лидер по успеваемости в {s.group} группе,'
+                                 f'{s.year} курса: {s.name}, средний балл {best_average_grade_in_group}\n')
+    return cont
+
+
 sorted_students_list = sort_students(students_list)
 
 # Выводим список студентов с именами, курсом, группой и годом рождения
 for st in sorted_students_list:
+    content += f'{st.name}, {st.year_of_birth} года рождения, {st.year} курс, {st.group} группа\n'
     print(f'{st.name}, {st.year_of_birth} года рождения, {st.year} курс, {st.group} группа')
 
-print_average_grade_by_group(students_list)
+content += print_average_grade_by_group(students_list)
 
+# Выводим старых студентов
+content += print_oldest_students(sorted_students_list)
 
+# Выводим самых молодых студентов
+content += print_young_students(sorted_students_list)
 
+# Ищем лучшего по успеваемости студента в группе
+content += print_best_student_in_group(sorted_students_list)
+
+with open(output_file_name, 'w', encoding='utf8') as output_file:
+    output_file.write(content.strip())
