7f8f82c6f5e398b50e07586b6a92eba5999ae955
AlekseyChirkin <ach1010110@yandex.ru>
2022-09-09 16:47:32 +0300

    delete re.py

--- re_practice.py
+++
@@ -1,9 +0,0 @@
-import  re
-
-file_name = 'Tasks/txt/textForTask_5.txt'
-
-with open(file_name, 'r', encoding='utf8') as in_file:
-    line = in_file.read()
-
-    r = re.compile(r'пис')
-    print(re.search(r, str))
