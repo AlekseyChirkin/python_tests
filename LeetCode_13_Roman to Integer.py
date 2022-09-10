23453814c13225c49bbf3f449222d7d1b1067b21
root <ach1010110@yandex.ru>
2022-09-10 23:46:50 +0300

    solve leetcode 13

--- LeetCode_13_Roman to Integer.py
+++ LeetCode_13_Roman to Integer.py
@@ -12,49 +12,39 @@
 
 def romanToInt(s: str) -> int:
     arabic_number = 0
-    for i in range(len(s)):
-        val = s[i]
-        if i + 1 < len(s):
-            next_val = s[i + 1]
-        if val == 'I' and i + 1 < len(s) and next_val == 'V':
-            arabic_number += 4
-            continue
-        elif val == 'I' and i + 1 < len(s) and next_val == 'X':
-            arabic_number += 9
-            continue
-        elif val == 'I':
-            arabic_number += 1
+    if s.count('IV') > 0:
+        arabic_number += s.count('IV') * 4
+        s = s.replace('IV', '')
+    if s.count('IX') > 0:
+        arabic_number += s.count('IX') * 9
+        s = s.replace('IX', '')
 
-        if val == 'X' and i + 1 < len(s) and next_val == 'L':
-            arabic_number += 40
-            continue
-        elif val == 'X' and i + 1 < len(s) and next_val == 'C':
-            arabic_number += 90
-            continue
-        elif val == 'X':
-            arabic_number += 10
+    if s.count('XL') > 0:
+        arabic_number += s.count('XL') * 40
+        s = s.replace('XL', '')
+    if s.count('XC') > 0:
+        arabic_number += s.count('XC') * 90
+        s = s.replace('XC', '')
 
-        if val == 'C' and i + 1 < len(s) and next_val == 'D':
-            arabic_number += 400
-            continue
-        elif val == 'C' and i + 1 < len(s) and next_val == 'M':
-            arabic_number += 900
-            continue
-        elif val == 'C':
-            arabic_number += 100
+    if s.count('CD') > 0:
+        arabic_number += s.count('CD') * 400
+        s = s.replace('CD', '')
+    if s.count('CM') > 0:
+        arabic_number += s.count('CM') * 900
+        s = s.replace('CM', '')
 
-        if val == 'V':
-            arabic_number += 5
-        if val == 'L':
-            arabic_number += 50
-        if val == 'D':
-            arabic_number += 500
-        if val == 'M':
-            arabic_number += 1000
+    for i in s:
+        if i == 'I': arabic_number += 1
+        if i == 'V': arabic_number += 5
+        if i == 'X': arabic_number += 10
+        if i == 'L': arabic_number += 50
+        if i == 'C': arabic_number += 100
+        if i == 'D': arabic_number += 500
+        if i == 'M': arabic_number += 1000
 
     return arabic_number
 
 
-roman_number = "LIV"
+roman_number = "MCMXCIV"
 
 print(romanToInt(roman_number))
