b0038c9316928e66311d8a7c5d1ea20b0945e3a5
Aleksey <ach1010110@yandex.ru>
2022-09-30 09:00:19 +0300

    solving problem 136

--- leetcode_and_algorithms_problems/136. Single Number.py
+++ leetcode_and_algorithms_problems/136. Single Number.py
@@ -1,28 +1,16 @@
 nums = [1, 0, 1]
+# nums = [4, 1, 2, 1, 2]
+#      [1,1,2,2,4]
+res = 0
 
-nums = sorted(nums)
-#      [0, 1, 1]
+dict1 = {}
 
-for i in range(len(nums)):
-    print(nums)
-    x1 = nums[i]
-    x2 = 0
+for num in nums:
+    dict1[num] = dict1.get(num, 0) + 1
 
-    l = i
-    r = len(nums) - 1
-    m = 0
+for key, val in dict1.items():
+    if val == 1:
+        res = key
+        break
 
-    while l < r:
-        m = (l + r) // 2
-        if nums[m] > nums[l]:
-            l = m + 1
-        elif nums[m] < nums[r]:
-            r = m
-        else:
-            x2 = nums[m]
-            break
-
-    if x1 == x2:
-        continue
-    else:
-        print(x1)
+print(res)
