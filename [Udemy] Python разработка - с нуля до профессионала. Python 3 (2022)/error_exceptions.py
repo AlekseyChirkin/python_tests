6e81fd0f646d8ad51d76dfdee34a7b1afea9e55e
Aleksey <ach1010110@yandex.ru>
2022-11-14 17:45:46 +0300

    testing value error printing

--- [Udemy] Python разработка - с нуля до профессионала. Python 3 (2022)/error_exceptions.py
+++ [Udemy] Python разработка - с нуля до профессионала. Python 3 (2022)/error_exceptions.py
@@ -5,7 +5,9 @@ while True:
             break
         y = int(x) ** 2
         print(f'Result is {y}')
-    except:
+    # except ValueError as te:
+    #     print(f'You must enter digits!\n{te}')
+    except BaseException:
         print('Enter correct number, please!')
         print('For exit type "exit"...\n')
 print('Program finished correct')
