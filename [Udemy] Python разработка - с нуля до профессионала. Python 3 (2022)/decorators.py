da1a8b7846c7f36d0aa2d3ade295fd3744c0b422
Aleksey <ach1010110@yandex.ru>
2022-11-21 16:21:46 +0300

    decorators learning

---
+++ [Udemy] Python разработка - с нуля до профессионала. Python 3 (2022)/decorators.py
@@ -0,0 +1,14 @@
+def decorator_func(orig_func):
+    def wrap_my_func():
+        print('additional code before')
+        orig_func()
+        print('additional code after')
+    return wrap_my_func()
+
+@decorator_func
+def my_func():
+    print('my function run')
+
+
+my_func
+
